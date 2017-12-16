5a0b2c8ed9a64c1ff590af97394e7d65773118ea
Jose Carlos <jotanaval2009@gmail.com>
2017-12-16 10:21:24 -0200

    endpoin

---
+++ src/main/java/br/com/endepoint/ProfessorEndPoint.java
@@ -0,0 +1,18 @@
+package br.com.endepoint;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("professor")
+public class ProfessorEndPoint {
+ 
+	
+	@GetMapping
+	public  ResponseEntity<?> hi(){
+		return new ResponseEntity<>("hi", HttpStatus.OK);
+	}
+}
